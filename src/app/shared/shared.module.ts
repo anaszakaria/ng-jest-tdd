08ba7bc5cb047676f093fe80d2e2c68880082885
Zakaria, Anas <anas.zakaria@sicpa.com>
2023-01-18 16:23:44 +0800

    Navbar Style

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlertComponent } from './alert/alert.component';
 import { ButtonComponent } from './button/button.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [AlertComponent, ButtonComponent],
+  declarations: [AlertComponent, ButtonComponent, NavbarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
-  exports: [AlertComponent, ButtonComponent]
+  exports: [AlertComponent, ButtonComponent, NavbarComponent]
 })
 export class SharedModule { }
